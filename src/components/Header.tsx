ebab264f1bd614dc5fdd8eadebf68012c69d1d0c
GLOIRE GWIZA <g.gwiza@alustudent.com>
2026-09-20 21:11:46 +0200

    feat: add Header component with text logo and New Post link

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <header className="header">
+      <div className="header__logo">Dev Insights</div>
+      <nav className="header__nav">
+        <a href="#new-post" className="header__link">
+          New Post
+        </a>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
